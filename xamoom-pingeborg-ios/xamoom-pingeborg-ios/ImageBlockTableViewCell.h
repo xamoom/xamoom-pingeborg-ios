bf90500bd4d5961a938aeec93862f7cba0ec7b51
Raphael Seher <raphael@xamoom.com>
2015-04-09 15:15:59 +0200

    Added pod and createt ImageBlock

---
+++ xamoom-pingeborg-ios/xamoom-pingeborg-ios/ImageBlockTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ImageBlockTableViewCell.h
+//  xamoom-pingeborg-ios
+//
+//  Created by Raphael Seher on 09/04/15.
+//  Copyright (c) 2015 xamoom GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageBlockTableViewCell : UITableViewCell
+
+@end
---
+++ xamoom-pingeborg-ios/xamoom-pingeborg-ios/ImageBlockTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  ImageBlockTableViewCell.m
+//  xamoom-pingeborg-ios
+//
+//  Created by Raphael Seher on 09/04/15.
+//  Copyright (c) 2015 xamoom GmbH. All rights reserved.
+//
+
+#import "ImageBlockTableViewCell.h"
+
+@implementation ImageBlockTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
