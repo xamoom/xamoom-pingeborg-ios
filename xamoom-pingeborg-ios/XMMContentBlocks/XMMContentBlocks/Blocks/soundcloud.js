c8fe3cd81ac3422ebad861d6b1027e602067a02c
Raphael Seher <raphael@xamoom.com>
2015-05-12 14:24:26 +0200

    Deleted some unused files

--- xamoom-pingeborg-ios/XMMContentBlocks/XMMContentBlocks/Blocks/soundcloud.js
+++
@@ -1,5 +0,0 @@
-<iframe width='100%' height='##height##' scrolling='no' frameborder='no'
-src='https://w.soundcloud.com/player/?url=##url##&auto_play=false&hide_related=true&show_comments=false&show_comments=false&show_user=false&show_reposts=false&sharing=false&download=false&buying=false&visual=true'></iframe>
-
-<script src="https://w.soundcloud.com/player/api.js" type="text/javascript"></script>
-
